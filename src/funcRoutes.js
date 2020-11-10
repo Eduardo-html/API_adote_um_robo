03aab3bd82486db58509bf913292667c54e0db92
Joaquim Castro <joaquimcastro2909@gmail.com>
2020-11-10 17:56:26 -0200

    order tweeking

--- src/funcRoutes.js
+++ src/funcRoutes.js
@@ -5,24 +5,6 @@ const automobScrape = require('./scrapping_files/automobilismo');
 const logisticScrape = require('./scrapping_files/logistica');
 const oleoGasScrape = require('./scrapping_files/oleo_gas');
 
-exports.veja = (app, rota) => {
-  const stockUri = {
-    varejo: 'sectorandindustry-sector/retail-trade',
-    saude: 'sectorandindustry-sector/health-services',
-    imobiliario: 'sectorandindustry-industry/homebuilding/'
-  }
-
-  app.get(`/${rota}`, async (req, res) => {
-    const response = await scrapper(`${rota}`)
-    const stocks = await stockScrape( stockUri[ rota ] )
-
-    res.json( {
-      newsList: response,
-      stockList: stocks
-    })
-  })
-}
-
 exports.automobScrape = ( app ) => {
   app.get('/automobilismo' , async ( req ,res ) => {
     const num = Math.round( Math.random() * 2 );
@@ -52,6 +34,24 @@ exports.oleoGasScrape = ( app ) => {
     const num = Math.round( Math.random() * 2 );
     const response = await oleoGasScrape[num]();
     const stocks = await stockScrape('sectorandindustry-industry/oil-gas-production')
+    res.json( {
+      newsList: response,
+      stockList: stocks
+    })
+  })
+}
+
+exports.veja = (app, rota) => {
+  const stockUri = {
+    varejo: 'sectorandindustry-sector/retail-trade',
+    saude: 'sectorandindustry-sector/health-services',
+    imobiliario: 'sectorandindustry-industry/homebuilding/'
+  }
+
+  app.get(`/${rota}`, async (req, res) => {
+    const response = await scrapper(`${rota}`)
+    const stocks = await stockScrape( stockUri[ rota ] )
+
     res.json( {
       newsList: response,
       stockList: stocks
