0b8312ee04d6f8956c6a6174c44db8dda3af8d06
Fabinnreis <fabioreisti@outlook.com>
2020-11-14 19:57:29 -0300

    Correcao do retorno do scrap e limpeza de codigo

--- src/scrapping_files/scraper-financas.js
+++ src/scrapping_files/scraper-financas.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 
-const scrapeG1 = async () => {
+const scrapeFinancas = async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(`https://investnews.com.br/financas/`);
@@ -8,9 +8,7 @@ const scrapeG1 = async () => {
     const result = await page.evaluate(()=>{
         const arr = [];
         document.querySelectorAll('li.mvp-blog-story-wrap')
-            .forEach( element => {
-                //arr.push( element.outerHTML )
-                
+            .forEach( element => {                
                     arr.push(
                         {
                             manchete: element.querySelector("div.mvp-blog-story-text > h2").innerText,
@@ -18,31 +16,7 @@ const scrapeG1 = async () => {
                             description: element.querySelector("div.mvp-blog-story-text > p").innerText,
                             img: element.querySelector("div.mvp-blog-story-img > img").getAttribute('src')
                           }
-                    )
-                    
-                    
-            });
-
-        return arr;
-    });
-    browser.close();
-
-  console.log(result);
-};
-
-scrapeG1();
-/* const scrapeFolha = async (param) => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(`https://search.folha.uol.com.br/search?q=${param}&periodo=todos&sd=&ed=&site=todos`);
-
-    const result = await page.evaluate(()=>{
-        const arr = [];
-        document.querySelectorAll('.u-list-unstyled')
-
-            .forEach( element => {
-                if ( element.children[0].children[0].textContent != "Ao Vivo - Mercado")
-                    arr.push( element.outerHTML )
+                    )                    
             });
 
         return arr;
@@ -51,16 +25,3 @@ scrapeG1();
 
   return result;
 };
-
-module.exports.scrapeG1 = scrapeG1;
-module.exports.scrapeFolha = scrapeFolha; */
-
-/* {
-    document.querySelectorAll('.bastian-feed-item > div > div')
-    manchete: element.querySelector(".feed-post-body-title gui-color-primary gui-color-hover ").innerText,
-    //link: element.querySelector(".feed-post-link gui-color-primary gui-color-hover").getAttribute('href'),
-    //description: element.querySelector(".feed-post-body-resumo").innerText,
-    //img: element.querySelector( "bstn-fd-picture-image").getAttribute('srcset')
-  } */
-
-  //if ( element.dataset.type != "cobertura" )
